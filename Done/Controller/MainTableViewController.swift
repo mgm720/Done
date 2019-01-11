0b66319688f707b053c78ff3a5d1851606064485
Michael Miles <michael.gage.miles@gmail.com>
2019-01-10 21:41:02 -0800

    Added fetchResultsController to sort data by Date

--- Done/Controller/MainTableViewController.swift
+++ Done/Controller/MainTableViewController.swift
@@ -11,12 +11,13 @@ import CoreData
 
 class MainTableViewController: UITableViewController {
     
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    var taskArray = [Task]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext   
+    var fetchController : NSFetchedResultsController<Task>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadData()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -25,16 +26,21 @@ class MainTableViewController: UITableViewController {
 
     // MARK: - Table view functions
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        if let frc = fetchController {
+            return frc.sections!.count
+        }
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return taskArray.count
+        return fetchController.sections?[section].numberOfObjects ?? 0
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Date"
+        guard let sectionInfo = fetchController?.sections?[section] else {
+            return nil
+        }
+        return sectionInfo.name
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -44,14 +50,16 @@ class MainTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as! TaskCell
 
-        cell.nameLabel.text = taskArray[indexPath.row].name ?? "Add Items"
+        let entity = fetchController.object(at: indexPath)
         
-        if taskArray[indexPath.row].dueTime == nil {
+        cell.nameLabel.text = entity.name ?? "Add Items"
+        
+        if entity.dueTime == nil {
             cell.timeLabel.text = ""
         } else {
             let timeFormatter = DateFormatter()
             timeFormatter.timeStyle = .short
-            cell.timeLabel.text = timeFormatter.string(from: taskArray[indexPath.row].dueTime!)
+            cell.timeLabel.text = timeFormatter.string(from: entity.dueTime!)
         }
 
         return cell
@@ -74,13 +82,15 @@ class MainTableViewController: UITableViewController {
     }
     
     func loadData() {
-        let request : NSFetchRequest<Task> = Task.fetchRequest()
+        let request = NSFetchRequest<Task>(entityName: "Task")
         let sort = NSSortDescriptor(key: "dueDate", ascending: false)
         let sort2 = NSSortDescriptor(key: "dueTime", ascending: false)
         request.sortDescriptors = [sort, sort2]
         
+        fetchController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: "dueDate", cacheName: nil)
+        
         do {
-            taskArray = try context.fetch(request)
+            try fetchController.performFetch()
         } catch {
             print("Error loading data \(error)")
         }
